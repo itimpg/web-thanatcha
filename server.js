c47ed733e1b849bca6e7809016b26fe7688dc354
Chatas Chairin <Chatas Chairin>
2016-07-12 00:49:23 +0700

    update main page

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
-
-app.use(express.static('./public'));
+app.set('view engine', 'html');
+app.set('views', 'public');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.render('index');
