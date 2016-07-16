4e68bd99d33e558f1922b0fdf791dc0144e8bca8
Chatas Chairin <Chatas Chairin>
2016-07-16 14:17:43 +0700

    update images for works

--- public/app/work/work.service.js
+++ public/app/work/work.service.js
@@ -1,95 +1,40 @@
 angular.module('work').factory('WorkService', [
     function () {
 
-        var images = [
-            {
-                year: 2012,
-                pictures: [
-                    '2012_01_01_opt.jpg',
-                    '2012_01_02_opt.jpg',
-                    '2012_01_03_opt.jpg',
-                    '2012_02_01_opt.jpg',
-                    '2012_02_02_opt.jpg',
-                    '2012_02_03_opt.jpg',
-                    '2012_03_01_opt.jpg',
-                    '2012_03_02_opt.jpg',
-                    '2012_03_03_opt.jpg',
-                ]
-            },
-            {
-                year: 2013,
-                pictures: [
-                    '2013_01_01_opt.jpg',
-                    '2013_01_02_opt.jpg',
-                    '2013_01_03_opt.jpg',
-                    '2013_01_04_opt.jpg',
-                    '2013_02_01_opt.jpg',
-                    '2013_02_02_opt.jpg',
-                    '2013_03_01_opt.jpg',
-                    '2013_03_02_opt.jpg',
-                    '2013_03_03_opt.jpg',
-                    '2013_03_04_opt.jpg',
-                    '2013_03_05_opt.jpg',
-                ]
-            },
-            {
-                year: 2014,
-                pictures: [
-                    '2014_01_01_opt.jpg',
-                    '2014_01_02_opt.jpg',
-                    '2014_01_03_opt.jpg',
-                    '2014_02_01_opt.jpg',
-                    '2014_03_01_opt.jpg',
-                ]
-            },
-            {
-                year: 2015,
-                pictures: [
-                    '2015_01_01_opt.jpg',
-                    '2015_01_02_opt.jpg',
-                    '2015_01_03_opt.jpg',
-                    '2015_01_04_opt.jpg',
-                    '2015_01_05_opt.jpg',
-                    '2015_01_06_opt.jpg',
-                    '2015_01_07_opt.jpg',
-                ]
-            }
-        ];
-
         var theDiminishFolkways = {
             id: 201601,
-            imageTitle: 'The Diminish Folkways/The diminish folkways (1).jpg',
+            imageTitle: 'The Diminish Folkways/The diminishing of the folkways 1.jpg',
             name: 'The Diminish Folkways',
             desc: 'clay, water, video',
-            dimension: 'Dimensins variable',
+            dimension: 'dimensions variable',
             images:
             [
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (1).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 1.jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (2).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 2.jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (3).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 3.jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (4).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 4.jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (5).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 5.jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (6).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 6.jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diminish Folkways/The diminish folkways (7).jpg',
+                    src: 'The Diminish Folkways/The diminishing of the folkways 7.jpg',
                     desc: "",
                 }
             ]
@@ -97,30 +42,30 @@ angular.module('work').factory('WorkService', [
 
         var theDiaryOfCraftsManI = {
             id: 201602,
-            imageTitle: 'The Diary of Craftsman I/The diary of craftmans I (3).jpg',
+            imageTitle: 'The Diary of Craftsman I/The diary of craftsman I (3).jpg',
             name: 'The Diary of Craftsman I',
             desc: 'craving on tools and table, photo',
             dimension: '80 x 120 x 200 cm',
             images:
             [
                 {
-                    src: 'The Diary of Craftsman I/The diary of craftmans I (1).jpg',
+                    src: 'The Diary of Craftsman I/The diary of craftsman I (1).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman I/The diary of craftmans I (2).jpg',
+                    src: 'The Diary of Craftsman I/The diary of craftsman I (2).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman I/The diary of craftmans I (3).jpg',
+                    src: 'The Diary of Craftsman I/The diary of craftsman I (3).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman I/The diary of craftmans I (4).jpg',
+                    src: 'The Diary of Craftsman I/The diary of craftsman I (4).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman I/The diary of craftmans I (5).jpg',
+                    src: 'The diary of craftsman I/The diary of craftsman I (5).jpg',
                     desc: "",
                 },
             ]
@@ -128,71 +73,67 @@ angular.module('work').factory('WorkService', [
 
         var theDiaryOfCraftsManII = {
             id: 201603,
-            imageTitle: 'The Diary of Craftsman II/The diary of craftmans II (1).jpg',
+            imageTitle: 'The Diary of Craftsman II/The diary of craftsman II (1).jpg',
             name: 'The Diary of Craftsman II',
             desc: 'craving tools and wood',
             dimension: '45, 135, 150 cm',
             images:
             [
                 {
-                    src: 'The Diary of Craftsman II/The diary of craftmans II (1).jpg',
+                    src: 'The Diary of Craftsman II/The diary of craftsman II (1).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman II/The diary of craftmans II (2).jpg',
+                    src: 'The Diary of Craftsman II/The diary of craftsman II (2).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman II/The diary of craftmans II (3).jpg',
+                    src: 'The Diary of Craftsman II/The diary of craftsman II (3).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman II/The diary of craftmans II (4).jpg',
+                    src: 'The Diary of Craftsman II/The diary of craftsman II (4).jpg',
                     desc: "",
                 },
                 {
-                    src: 'The Diary of Craftsman II/The diary of craftmans II (5).jpg',
+                    src: 'The Diary of Craftsman II/The diary of craftsman II (5).jpg',
                     desc: "",
                 },
             ]
         };
 
-        var theSomething1 = {
+        var theCheckMate = {
             id: 201604,
-            imageTitle: 'untitle1/I (6).jpg',
+            imageTitle: 'Check - mate/check-mate (3).jpg',
             name: 'Check - mate',
             desc: 'Rubbing on paper, Drawing, wood',
-            dimension: 'Dimensins variable',
+            dimension: 'dimensions variable',
             images:
             [
                 {
-                    src: 'untitle1/I (1).jpg',
+                    src: 'Check - mate/check-mate (1).jpg',
                     desc: "",
                 },
                 {
-                    src: 'untitle1/I (2).jpg',
+                    src: 'Check - mate/check-mate (2).jpg',
                     desc: "",
                 },
                 {
-                    src: 'untitle1/I (3).jpg',
+                    src: 'Check - mate/check-mate (3).jpg',
                     desc: "",
                 },
                 {
-                    src: 'untitle1/I (4).jpg',
+                    src: 'Check - mate/check-mate (4).jpg',
                     desc: "",
                 },
                 {
-                    src: 'untitle1/I (5).jpg',
+                    src: 'Check - mate/check-mate (5).jpg',
                     desc: "",
-                },
-                {
-                    src: 'untitle1/I (6).jpg',
-                    desc: "",
-                },
+                }
             ]
         };
 
-        var theSomething2 = {
+        var theUntitle2 = {
             id: 201605,
             imageTitle: 'untitle2/4.jpg',
             name: 'The Untitle II',
@@ -218,6 +159,38 @@ angular.module('work').factory('WorkService', [
             ]
         };
 
+        var howToUseTheseTools = {
+            id: 201501,
+            imageTitle: 'How to use these tools/How to use these tools 1.jpg',
+            name: 'How to use these tools',
+            desc: '',
+            dimension: '',
+            images:
+            [
+                {
+                    src: 'How to use these tools/How to use these tools 1.jpg',
+                    desc: "",
+                },
+                {
+                    src: 'How to use these tools/How to use these tools 2.jpg',
+                    desc: "",
+                },
+                {
+                    src: 'How to use these tools/How to use these tools 3.jpg',
+                    desc: "",
+                },
+                {
+                    src: 'How to use these tools/How to use these tools 4.jpg',
+                },
+                {
+                    src: 'How to use these tools/How to use these tools 5.jpg',
+                },
+                {
+                    src: 'How to use these tools/How to use these tools 6.jpg',
+                }
+            ]
+        };
+
         var works = [
             {
                 year: 2016,
@@ -226,10 +199,16 @@ angular.module('work').factory('WorkService', [
                     theDiminishFolkways,
                     theDiaryOfCraftsManII,
                     theDiaryOfCraftsManI,
-                    theSomething2,
-                    theSomething1
+                    theUntitle2,
+                    theCheckMate
                 ]
             },
+            {
+                year: 2015,
+                projects: [
+                    howToUseTheseTools
+                ]
+            }
         ];
 
 
